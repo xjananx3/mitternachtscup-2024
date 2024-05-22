ad8eee906d9853c42683676a642eafbf6e44c5fb
Jan <janan9980@gmail.com>
2024-05-23 00:30:00 +0200

    Erstelle Enum Platten

---
+++ src/MitternachtsCup/Data/Enum/Platten.cs
@@ -0,0 +1,12 @@
+namespace MitternachtsCup.Data.Enum;
+
+public enum Platten
+{
+    Tisch1 = 1,
+    Tisch2 = 2,
+    Tisch3 = 3,
+    Tisch4 = 4,
+    Tisch5 = 5,
+    Tisch6 = 6,
+    Ausstehend = 7
+}
