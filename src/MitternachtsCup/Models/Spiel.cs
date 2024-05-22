a68316baa29073eb39dbca15ff535b2597637973
Jan <janan9980@gmail.com>
2024-05-23 00:27:26 +0200

    Erstelle Spiel-Model

---
+++ src/MitternachtsCup/Models/Spiel.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MitternachtsCup.Models;
+
+public class Spiel
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public Platten Platte { get; set; }
+    public DateTime StartZeit { get; set; }
+    public TimeSpan SpielDauer { get; set; }
+    public int TeamAId { get; set; }
+    public Team TeamA { get; set; }
+    public int TeamBId { get; set; }
+    public Team TeamB { get; set; }
+}
