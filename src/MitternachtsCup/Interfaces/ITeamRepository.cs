907a40175d306da42a17cc7f3d53d1d713b8818b
Jan <janan9980@gmail.com>
2024-07-20 01:07:19 +0200

    Added TeamRepo with Interface with CRUD Operations

---
+++ src/MitternachtsCup/Interfaces/ITeamRepository.cs
@@ -0,0 +1,13 @@
+using MitternachtsCup.Models;
+
+namespace MitternachtsCup.Interfaces;
+
+public interface ITeamRepository
+{
+    Task<IEnumerable<Team>> GetAll();
+    Task<Team> GetByIdAsync(int id);
+    bool Add(Team team);
+    bool Update(Team team);
+    bool Delete(Team team);
+    bool Save();
+}
