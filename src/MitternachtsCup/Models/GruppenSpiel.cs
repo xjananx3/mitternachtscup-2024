2823cf3c723580c4ca71c07ed5fd0338679f7522
Jan <janan9980@gmail.com>
2024-07-27 14:58:18 +0200

    (Gruppenspiel-View)added grppenspiele

---
+++ src/MitternachtsCup/Models/GruppenSpiel.cs
@@ -0,0 +1,7 @@
+namespace MitternachtsCup.Models;
+
+public class GruppenSpiel
+{
+    public Team TeamA { get; set; }
+    public Team TeamB { get; set; }
+}
--- src/MitternachtsCup/ViewModels/GruppenAnzeigeViewModel.cs
+++ src/MitternachtsCup/ViewModels/GruppenAnzeigeViewModel.cs
@@ -12,5 +12,14 @@ public class GruppenAnzeigeViewModel
     public List<Team> GruppeFTeams { get; set; } = new();
     public List<Team> GruppeGTeams { get; set; } = new();
     public List<Team> GruppeHTeams { get; set; } = new();
-    
+
+    public List<GruppenSpiel> GruppeASpiele { get; set; } = new List<GruppenSpiel>();
+    public List<GruppenSpiel> GruppeBSpiele { get; set; } = new List<GruppenSpiel>();
+    public List<GruppenSpiel> GruppeCSpiele { get; set; } = new List<GruppenSpiel>();
+    public List<GruppenSpiel> GruppeDSpiele { get; set; } = new List<GruppenSpiel>();
+    public List<GruppenSpiel> GruppeESpiele { get; set; } = new List<GruppenSpiel>();
+    public List<GruppenSpiel> GruppeFSpiele { get; set; } = new List<GruppenSpiel>();
+    public List<GruppenSpiel> GruppeGSpiele { get; set; } = new List<GruppenSpiel>();
+    public List<GruppenSpiel> GruppeHSpiele { get; set; } = new List<GruppenSpiel>();
+
 }
