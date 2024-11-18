4400953231e2d86ac78e73405d28e30c2d256a0f
jack <janan9980@gmail.com>
2024-11-18 02:20:50 +0100

    override model set delateBehaviour NoAction

--- src/MitternachtsCup/Data/ApplicationDbContext.cs
+++ src/MitternachtsCup/Data/ApplicationDbContext.cs
@@ -12,5 +12,22 @@ public class ApplicationDbContext : DbContext
 
     public DbSet<Team> Teams { get; set; }
     public DbSet<Spiel> Spiele { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Spiel>()
+            .HasOne(s => s.TeamB)
+            .WithMany()
+            .HasForeignKey(s => s.TeamBId)
+            .OnDelete(DeleteBehavior.NoAction); 
+
+        modelBuilder.Entity<Spiel>()
+            .HasOne(s => s.TeamB)
+            .WithMany()
+            .HasForeignKey(s => s.TeamBId)
+            .OnDelete(DeleteBehavior.NoAction); // oder DeleteBehavior.NoAction
+    }
+    
     
 }
